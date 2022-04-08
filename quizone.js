bb9ee62bc0c23f1d341d721c3f499a58ab212717
nomanmangalzai <62675080+nomanmangalzai@users.noreply.github.com>
2022-04-09 04:23:54 +0500

    adding javascript to quiz1

---
+++ quizone.js
@@ -0,0 +1,16 @@
+function changePic(){
+    console.log("the function works");
+//var icon = document.getElementById("icon01");
+var icon = $("#icon01");
+icon.src = "../images/noman02.JPEG";
+}
+
+/*
+function changePic(){
+    var changePicture = $("#icon01");
+    changePicture.src = "../images/noman02.JPEG";
+
+
+}
+window.onload = changePic;
+*/
